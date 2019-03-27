3f8b9c1bbd561b7e2d5760fffc7adf7ce2bc3717
Luke Chandler <lhchandler4@gmail.com>
2019-03-26 18:04:57 -0700

    updated css and html

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@
 var myState = prompt('Did Luke spend most of his life in Tennessee');
 if(myState === 'yes' || myState === 'Yes' || myState === 'Y' || myState === 'y'){
   alert('You are right! He spent his first 22 years there.');
+  console.log('You\'re right!')
 } 
 else if(myState === 'no' || myState === 'No' || myState === 'N' || myState === 'n'){
   alert('Nope. He did spend most of his life in Tennessee.');
+  console.log('You guessed wrong')
 }
 else{
   console.log('Please answer either Yes or No. All questions follow Yes/No format!')
@@ -14,39 +16,62 @@ else{
 var myChild = prompt('Is Luke an only child?');
 if(myChild === 'yes' || myChild === 'Yes' || myChild === 'Y' || myChild === 'y'){
   alert('Nope. He has 2 sisters and 1 brother.');
+  console.log('You guessed wrong')
 } else if(myChild === 'no' || myChild === 'No' || myChild === 'N' || myChild === 'n'){
   alert('You are right. He is one of 4 children.');
+  console.log('You\'re right!')
 }
 else{
-  console.log('As I told you in question 1, all questions follow Yes/No format for answers')
+  console.log('Yes or No answers please!')
 }
 
 var myLang = prompt('Does Luke speak a foreign language?');
 if(myLang === 'yes' || myLang === 'Yes' || myLang === 'Y' || myLang === 'y'){
   alert('You are correct. He speaks Korean');
+  console.log('You\'re right!')
 } else if(myLang === 'no' || myLang === 'No' || myLang === 'N' || myLang === 'n'){
   alert('Nah. He does speak a foreign language. Korean!');
+  console.log('You guessed wrong')
 }
 else{
-  console.log('Come on. Really?')
+  console.log('Yes or No answers please!')
 }
 
 var myPet = prompt('Does Luke have a pet at home?');
 if(myPet === 'yes' || myPet === 'Yes' || myPet === 'Y' || myPet === 'y'){
   alert('No.... He does\'t have a pet but really wants a cat and dog!');
+  console.log('You guessed wrong')
 } else if(myPet === 'no' || myPet === 'No' || myPet === 'N' || myPet === 'n'){
   alert('Unfortunately you\'re right... No pets as of yet...');
+  console.log('You\'re right!')
 }
 else{
-  console.log('I told you to answer Yes or No!')
+  console.log('Yes or No answers please!')
 }
 
+//I turned this section into a commit to utilize .toUpperCase() in the next section
+//var myMtn = prompt('Is Luke\'s favorite hike around Seattle Mt. Ranier?');
+//if(myMtn === 'yes' || myMtn === 'Yes' || myMtn === 'Y' || myMtn === 'y'){
+//  alert('No, he hasn\'t had a chance to do any hikes in the area yet...');
+//} else if(myMtn === 'no' || myMtn === 'No' || myMtn === 'N' || myMtn === 'n'){
+//  alert('You\'re right. He hasn\'t had a chance to do any hikes in the area ////yet...');
+//}
+//else{
+//  console.log('Yes or No answers please!')
+//}
+
 var myMtn = prompt('Is Luke\'s favorite hike around Seattle Mt. Ranier?');
-if(myMtn === 'yes' || myMtn === 'Yes' || myMtn === 'Y' || myMtn === 'y'){
+var myMtn2 = myMtn.toUpperCase();
+  if (myMtn2 === 'YES' || myMtn2 === 'Y'){
   alert('No, he hasn\'t had a chance to do any hikes in the area yet...');
-} else if(myMtn === 'no' || myMtn === 'No' || myMtn === 'N' || myMtn === 'n'){
-  alert('You\'re right. He hasn\'t had a chance to do any hikes in the area yet...');
+  console.log('You guessed wrong')
+} else if(myMtn2 === ''){
+  alert('You have to answer Yes or No')
+}
+  else if(myMtn2 === 'NO' || myMtn2 === 'N'){
+  alert('You\'re right. He hasn\'t had a chance to do any hikes in the area ////yet...');
+  console.log('You\'re right!')
 }
 else{
-  console.log('I can\'t repeat myself again. Luckily this was the last question')
+  console.log('Yes or No answers please!')
 }
